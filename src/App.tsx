ebe50e4abd182b1d58732eafb8ed4b62c53c859d
Thalita dos Reis <dosreistha@gmail.com>
2023-03-15 14:29:10 +0100

    defined layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
