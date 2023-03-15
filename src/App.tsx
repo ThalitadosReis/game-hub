8d509d576c018a4ec3ef22387ce8274f387af740
Thalita dos Reis <dosreistha@gmail.com>
2023-03-15 14:16:23 +0100

    installed chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Button colorScheme="blue">Button</Button>
     </div>
-  )
+  );
 }
 
 export default App;
